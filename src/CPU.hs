474921ab1c59584bb7cbdce08312451be745a333
Nick Chapman <nic@amadido.co.uk>
2026-02-11 09:03:34 +0000

    rejig arg fetch & trace

--- src/CPU.hs
+++ src/CPU.hs
@@ -39,185 +39,89 @@ maybeHalt Config{stop_at} = do
       when (cycles > max) Halt
 
 executeOpcode :: Config -> State -> U8 -> Eff Int
-executeOpcode Config{trace} s opcode = do
+executeOpcode Config{trace} s@State{ip} opcode = do
   let (instruction,cycles,mode) = decode opcode
   let withArg = executeWithArg s instruction
-  (bytes,strArg,eff) <- doMode s mode withArg
-  when (trace) $ logCpuInstruction s (opcode:bytes) instruction mode strArg
-  let n = 1 + length bytes
-  advanceIP n s
+  (addr,eff) <- doMode s mode withArg
+  when (trace) $ logCpuInstruction s instruction mode addr
+  let n = 1 + sizeMode mode
+  update (+ fromIntegral n) ip
   eff
   pure cycles
 
+doMode :: State -> Mode -> WithArg -> Eff (Addr, Eff ())
+doMode s@State{bus} mode = \case
+  Arg0 eff -> pure (undefined,eff)
+  Arg1 f -> do
+    addr <- fetchArgs s mode
+    let
+      eff = do
+        byte <- read (bus addr)
+        f byte
+    pure (addr,eff)
+  Arg2 f -> do
+    addr <- fetchArgs s mode
+    let eff = f addr
+    pure (addr,eff)
+
+fetchArgs :: State -> Mode -> Eff Addr
+fetchArgs State{ip,bus} = \case
+
+  Immediate -> do
+    pc <- read ip
+    let addr = pc + 1  -- TODO: weird?
+    pure addr
+
+  Absolute -> do
+    pc <- read ip
+    lo <- read (bus (pc+1))
+    hi <- read (bus (pc+2))
+    let addr = makeAddr HL {hi,lo}
+    pure addr
+
+  ZeroPage -> do
+    pc <- read ip
+    lo <- read (bus (pc+1))
+    let addr = makeAddr HL { hi = 0, lo }
+    pure addr
+
+  Relative -> do
+    pc <- read ip
+    off <- read (bus (pc+1))
+    let dist :: Int = (if off < 128 then fromIntegral off else fromIntegral off - 256)
+    let addr = 2 + pc + fromIntegral dist
+    pure addr
+
+  mode ->
+    error $ printf "Unimplemented addressing mode: %s" (show mode)
+
 ----------------------------------------------------------------------
--- instructions
+-- trace instruction
 
-data Instruction
-  = ADC | AND | ASL | BCC | BCS | BEQ | BIT | BMI
-  | BNE | BPL | BRK | BVC | BVS | CLC | CLD | CLI
-  | CLV | CMP | CPX | CPY | DEC | DEX | DEY | EOR
-  | INC | INX | INY | JMP | JSR | LDA | LDX | LDY
-  | LSR | NOP | ORA | PHA | PHP | PLA | PLP | ROL
-  | ROR | RTI | RTS | SBC | SEC | SED | SEI | STA
-  | STX | STY | TAX | TAY | TSX | TXA | TXS | TYA
-  deriving (Eq,Show)
+logCpuInstruction :: State -> Instruction -> Mode -> Addr -> Eff ()
+logCpuInstruction s@State{bus,ip} instruction mode addr = do
+  pc <- read ip
+  opcode <- read (bus (pc))
+  args <- sequence [ read (bus (pc + fromIntegral i)) | i <- [ 1 .. sizeMode mode ] ]
+  let bytes = opcode:args
+  let bytesS = intercalate " " (map (printf "%02X") bytes)
+  let a = printf "%s  %s %s" (ljust 8 bytesS) (show instruction) (seeArgs (mode,args,addr))
+  b <- seeState s
+  Log $ printf "%04X  %s%s" pc (ljust 42 a) b
 
-decode :: U8 -> (Instruction,Int,Mode)
-decode = \case
+ljust :: Int -> String -> String
+ljust n s = s <> take (max 0 (n - length s)) (repeat ' ')
 
-  0x69 -> ( ADC, 2, Immediate )
-  0x65 -> ( ADC, 3, ZeroPage )
-  0x6d -> ( ADC, 4, Absolute )
-  0x7d -> ( ADC, 4, AbsoluteX )
-  0x79 -> ( ADC, 4, AbsoluteY )
-  0x75 -> ( ADC, 4, ZeroPageX )
-  0x71 -> ( ADC, 5, IndirectIndexedY )
-  0x61 -> ( ADC, 6, IndexedIndirectX )
-  0x29 -> ( AND, 2, Immediate )
-  0x25 -> ( AND, 3, ZeroPage )
-  0x2d -> ( AND, 4, Absolute )
-  0x3d -> ( AND, 4, AbsoluteX )
-  0x39 -> ( AND, 4, AbsoluteY )
-  0x35 -> ( AND, 4, ZeroPageX )
-  0x31 -> ( AND, 5, IndirectIndexedY )
-  0x21 -> ( AND, 6, IndexedIndirectX )
-  0x0a -> ( ASL, 2, Accumulator )
-  0x06 -> ( ASL, 5, ZeroPage )
-  0x0e -> ( ASL, 6, Absolute )
-  0x16 -> ( ASL, 6, ZeroPageX )
-  0x1e -> ( ASL, 7, AbsoluteX )
-  0x90 -> ( BCC, 2, Relative )
-  0xb0 -> ( BCS, 2, Relative )
-  0xf0 -> ( BEQ, 2, Relative )
-  0x24 -> ( BIT, 3, ZeroPage )
-  0x2c -> ( BIT, 4, Absolute )
-  0x30 -> ( BMI, 2, Relative )
-  0xd0 -> ( BNE, 2, Relative )
-  0x10 -> ( BPL, 2, Relative )
-  0x00 -> ( BRK, 7, Implied )
-  0x50 -> ( BVC, 2, Relative )
-  0x70 -> ( BVS, 2, Relative )
-  0x18 -> ( CLC, 2, Implied )
-  0xd8 -> ( CLD, 2, Implied )
-  0x58 -> ( CLI, 2, Implied )
-  0xb8 -> ( CLV, 2, Implied )
-  0xc9 -> ( CMP, 2, Immediate )
-  0xc5 -> ( CMP, 3, ZeroPage )
-  0xcd -> ( CMP, 4, Absolute )
-  0xdd -> ( CMP, 4, AbsoluteX )
-  0xd9 -> ( CMP, 4, AbsoluteY )
-  0xd5 -> ( CMP, 4, ZeroPageX )
-  0xd1 -> ( CMP, 5, IndirectIndexedY )
-  0xc1 -> ( CMP, 6, IndexedIndirectX )
-  0xe0 -> ( CPX, 2, Immediate )
-  0xe4 -> ( CPX, 3, ZeroPage )
-  0xec -> ( CPX, 4, Absolute )
-  0xc0 -> ( CPY, 2, Immediate )
-  0xc4 -> ( CPY, 3, ZeroPage )
-  0xcc -> ( CPY, 4, Absolute )
-  0xc6 -> ( DEC, 5, ZeroPage )
-  0xce -> ( DEC, 6, Absolute )
-  0xd6 -> ( DEC, 6, ZeroPageX )
-  0xde -> ( DEC, 7, AbsoluteX )
-  0xca -> ( DEX, 2, Implied )
-  0x88 -> ( DEY, 2, Implied )
-  0x49 -> ( EOR, 2, Immediate )
-  0x45 -> ( EOR, 3, ZeroPage )
-  0x4d -> ( EOR, 4, Absolute )
-  0x5d -> ( EOR, 4, AbsoluteX )
-  0x59 -> ( EOR, 4, AbsoluteY )
-  0x55 -> ( EOR, 4, ZeroPageX )
-  0x51 -> ( EOR, 5, IndirectIndexedY )
-  0x41 -> ( EOR, 6, IndexedIndirectX )
-  0xe6 -> ( INC, 5, ZeroPage )
-  0xee -> ( INC, 6, Absolute )
-  0xf6 -> ( INC, 6, ZeroPageX )
-  0xfe -> ( INC, 7, AbsoluteX )
-  0xe8 -> ( INX, 2, Implied )
-  0xc8 -> ( INY, 2, Implied )
-  0x4c -> ( JMP, 3, Absolute )
-  0x6c -> ( JMP, 5, Indirect )
-  0x20 -> ( JSR, 6, Absolute )
-  0xa9 -> ( LDA, 2, Immediate )
-  0xa5 -> ( LDA, 3, ZeroPage )
-  0xad -> ( LDA, 4, Absolute )
-  0xbd -> ( LDA, 4, AbsoluteX )
-  0xb9 -> ( LDA, 4, AbsoluteY )
-  0xb5 -> ( LDA, 4, ZeroPageX )
-  0xb1 -> ( LDA, 5, IndirectIndexedY )
-  0xa1 -> ( LDA, 6, IndexedIndirectX )
-  0xa2 -> ( LDX, 2, Immediate )
-  0xa6 -> ( LDX, 3, ZeroPage )
-  0xae -> ( LDX, 4, Absolute )
-  0xbe -> ( LDX, 4, AbsoluteY )
-  0xb6 -> ( LDX, 4, ZeroPageY )
-  0xa0 -> ( LDY, 2, Immediate )
-  0xa4 -> ( LDY, 3, ZeroPage )
-  0xac -> ( LDY, 4, Absolute )
-  0xbc -> ( LDY, 4, AbsoluteX )
-  0xb4 -> ( LDY, 4, ZeroPageX )
-  0x4a -> ( LSR, 2, Accumulator )
-  0x46 -> ( LSR, 5, ZeroPage )
-  0x4e -> ( LSR, 6, Absolute )
-  0x56 -> ( LSR, 6, ZeroPageX )
-  0x5e -> ( LSR, 7, AbsoluteX )
-  0xea -> ( NOP, 2, Implied )
-  0x09 -> ( ORA, 2, Immediate )
-  0x05 -> ( ORA, 3, ZeroPage )
-  0x0d -> ( ORA, 4, Absolute )
-  0x1d -> ( ORA, 4, AbsoluteX )
-  0x19 -> ( ORA, 4, AbsoluteY )
-  0x15 -> ( ORA, 4, ZeroPageX )
-  0x11 -> ( ORA, 5, IndirectIndexedY )
-  0x01 -> ( ORA, 6, IndexedIndirectX )
-  0x48 -> ( PHA, 3, Implied )
-  0x08 -> ( PHP, 3, Implied )
-  0x68 -> ( PLA, 4, Implied )
-  0x28 -> ( PLP, 4, Implied )
-  0x2a -> ( ROL, 2, Accumulator )
-  0x26 -> ( ROL, 5, ZeroPage )
-  0x2e -> ( ROL, 6, Absolute )
-  0x36 -> ( ROL, 6, ZeroPageX )
-  0x3e -> ( ROL, 7, AbsoluteX )
-  0x6a -> ( ROR, 2, Accumulator )
-  0x66 -> ( ROR, 5, ZeroPage )
-  0x6e -> ( ROR, 6, Absolute )
-  0x76 -> ( ROR, 6, ZeroPageX )
-  0x7e -> ( ROR, 7, AbsoluteX )
-  0x40 -> ( RTI, 6, Implied )
-  0x60 -> ( RTS, 6, Implied )
-  0xe9 -> ( SBC, 2, Immediate )
-  0xe5 -> ( SBC, 3, ZeroPage )
-  0xed -> ( SBC, 4, Absolute )
-  0xfd -> ( SBC, 4, AbsoluteX )
-  0xf9 -> ( SBC, 4, AbsoluteY )
-  0xf5 -> ( SBC, 4, ZeroPageX )
-  0xf1 -> ( SBC, 5, IndirectIndexedY )
-  0xe1 -> ( SBC, 6, IndexedIndirectX )
-  0x38 -> ( SEC, 2, Implied )
-  0xf8 -> ( SED, 2, Implied )
-  0x78 -> ( SEI, 2, Implied )
-  0x85 -> ( STA, 3, ZeroPage )
-  0x8d -> ( STA, 4, Absolute )
-  0x95 -> ( STA, 4, ZeroPageX )
-  0x9d -> ( STA, 5, AbsoluteX )
-  0x99 -> ( STA, 5, AbsoluteY )
-  0x81 -> ( STA, 6, IndexedIndirectX )
-  0x91 -> ( STA, 6, IndirectIndexedY )
-  0x86 -> ( STX, 3, ZeroPage )
-  0x8e -> ( STX, 4, Absolute )
-  0x96 -> ( STX, 4, ZeroPageY )
-  0x84 -> ( STY, 3, ZeroPage )
-  0x8c -> ( STY, 4, Absolute )
-  0x94 -> ( STY, 4, ZeroPageX )
-  0xaa -> ( TAX, 2, Implied )
-  0xa8 -> ( TAY, 2, Implied )
-  0xba -> ( TSX, 2, Implied )
-  0x8a -> ( TXA, 2, Implied )
-  0x9a -> ( TXS, 2, Implied )
-  0x98 -> ( TYA, 2, Implied )
-  opcode ->
-    error (printf "unknown opcode: %02x" opcode)
+seeArgs :: (Mode,[U8],Addr) -> String
+seeArgs = \case
+  (Implied,[],_) -> ""
+  (Accumulator,[],_) -> "A"
+  (Immediate,[b1],_) -> printf "#$%02X" b1
+  (ZeroPage,[b1],_) -> printf "$%02X" b1
+  (Absolute,[_,_],addr) -> printf "$%04X" addr
+  (Relative,[_],addr) -> printf "$%04X" addr
+  (mode,bytes,_) ->
+    error $ printf "seeArgs:%s/%s" (show mode) (show bytes)
 
 ----------------------------------------------------------------------
 -- CpuBus
@@ -281,108 +185,39 @@ seeState State{a,x,y,flags,sp} = do
   pure mes
 
 ----------------------------------------------------------------------
--- execute
-
-doMode :: State -> Mode -> WithArg -> Eff ([U8], String, Eff ())
-doMode s@State{bus} mode = \case
-  Arg0 eff -> pure ([], "", eff)
-  Arg1 f -> do
-    (bytes,view,addr) <- fetchArgs s mode
-    let
-      eff = do
-        byte <- read (bus addr)
-        f byte
-    pure (bytes,view,eff)
-  Arg2 f -> do
-    (bytes,view,addr) <- fetchArgs s mode
-    let eff = f addr
-    pure (bytes,view,eff)
-
-advanceIP :: Int -> State -> Eff ()
-advanceIP n State{ip} =
-  update (+ fromIntegral n) ip
-
-----------------------------------------------------------------------
--- trace instruction
-
-logCpuInstruction :: State -> [U8] -> Instruction -> Mode -> String -> Eff ()
-logCpuInstruction s@State{ip} bytes instruction mode strArg = do
-  pc <- read ip
-  let bytesS = intercalate " " (map (printf "%02X") bytes)
-  let modeA = if mode == Accumulator then " A" else ""
-  let a = printf "%s  %s%s %s" (ljust 8 bytesS) (show instruction) modeA strArg
-  b <- seeState s
-  Log $ printf "%04X  %s%s" pc (ljust 42 a) b
-
-ljust :: Int -> String -> String
-ljust n s = s <> take (max 0 (n - length s)) (repeat ' ')
-
-----------------------------------------------------------------------
 -- addressing modes
 
 data Mode
-    = Immediate
-    | ZeroPage
-    | Absolute
-    | Implied
-    | Relative
-    | AbsoluteX
-    | AbsoluteY
-    | ZeroPageX
-    | ZeroPageY
-    | IndexedIndirectX
-    | IndirectIndexedY
-    | Accumulator
-    | Indirect
-    deriving (Eq,Show)
+  = Immediate
+  | ZeroPage
+  | Absolute
+  | Implied
+  | Relative
+  | AbsoluteX
+  | AbsoluteY
+  | ZeroPageX
+  | ZeroPageY
+  | IndexedIndirectX
+  | IndirectIndexedY
+  | Accumulator
+  | Indirect
+  deriving (Eq,Show)
 
-fetchArgs :: State -> Mode -> Eff ([U8],String,Addr)
-fetchArgs State{ip,bus} = \case
-  Absolute -> do
-    pc <- read ip
-    lo <- read (bus (pc+1))
-    hi <- read (bus (pc+2))
-    let addr = makeAddr HL {hi,lo}
-    let view = printf "$%04X" addr
-    pure ([lo,hi], view, addr)
-
-  Immediate -> do
-    pc <- read ip
-    lo <- read (bus (pc+1))
-    let bytes = [lo]
-    let view = printf "#$%02X" lo
-    let addr = pc + 1  -- TODO: weird?
-    pure (bytes, view, addr)
-
-  ZeroPage -> do
-    pc <- read ip
-    lo <- read (bus (pc+1))
-    let bytes = [lo]
-    let view = printf "$%02X" lo
-    let addr = makeAddr HL { hi = 0, lo }
-    pure (bytes, view, addr)
-
-{-
-  ZeroPageX -> do
-    pc <- read ip
-    b1 <- read (bus (pc+1))
-    let bytes = [b1]
-    let view = printf "$%02X, x" b1
-    x <- read x
-    let addr = makeAddr HL { hi = 0, lo = b1 + x}
-    pure (bytes, view, addr)
--}
-  Relative -> do
-    pc <- read ip
-    off <- read (bus (pc+1))
-    let dist :: Int = (if off < 128 then fromIntegral off else fromIntegral off - 256)
-    let bytes = [off]
-    let addr = 2 + pc + fromIntegral dist
-    let view = printf "$%04X" addr
-    pure (bytes, view, addr)
-
-  mode ->
-    error $ printf "Unimplemented addressing mode: %s" (show mode)
+sizeMode :: Mode -> Int
+sizeMode = \case
+  Immediate -> 1
+  ZeroPage -> 1
+  Absolute -> 2
+  Implied -> 0
+  Relative -> 1
+  AbsoluteX -> undefined
+  AbsoluteY -> undefined
+  ZeroPageX -> undefined
+  ZeroPageY -> undefined
+  IndexedIndirectX -> undefined
+  IndirectIndexedY -> undefined
+  Accumulator -> 0
+  Indirect -> undefined
 
 ----------------------------------------------------------------------
 
@@ -648,3 +483,173 @@ isNegative v = v `testBit` 7
 
 isPositive :: U8 -> Bool
 isPositive = not . isNegative
+
+----------------------------------------------------------------------
+-- instructions
+
+data Instruction
+  = ADC | AND | ASL | BCC | BCS | BEQ | BIT | BMI
+  | BNE | BPL | BRK | BVC | BVS | CLC | CLD | CLI
+  | CLV | CMP | CPX | CPY | DEC | DEX | DEY | EOR
+  | INC | INX | INY | JMP | JSR | LDA | LDX | LDY
+  | LSR | NOP | ORA | PHA | PHP | PLA | PLP | ROL
+  | ROR | RTI | RTS | SBC | SEC | SED | SEI | STA
+  | STX | STY | TAX | TAY | TSX | TXA | TXS | TYA
+  deriving (Eq,Show)
+
+decode :: U8 -> (Instruction,Int,Mode)
+decode = \case
+
+  0x69 -> ( ADC, 2, Immediate )
+  0x65 -> ( ADC, 3, ZeroPage )
+  0x6d -> ( ADC, 4, Absolute )
+  0x7d -> ( ADC, 4, AbsoluteX )
+  0x79 -> ( ADC, 4, AbsoluteY )
+  0x75 -> ( ADC, 4, ZeroPageX )
+  0x71 -> ( ADC, 5, IndirectIndexedY )
+  0x61 -> ( ADC, 6, IndexedIndirectX )
+  0x29 -> ( AND, 2, Immediate )
+  0x25 -> ( AND, 3, ZeroPage )
+  0x2d -> ( AND, 4, Absolute )
+  0x3d -> ( AND, 4, AbsoluteX )
+  0x39 -> ( AND, 4, AbsoluteY )
+  0x35 -> ( AND, 4, ZeroPageX )
+  0x31 -> ( AND, 5, IndirectIndexedY )
+  0x21 -> ( AND, 6, IndexedIndirectX )
+  0x0a -> ( ASL, 2, Accumulator )
+  0x06 -> ( ASL, 5, ZeroPage )
+  0x0e -> ( ASL, 6, Absolute )
+  0x16 -> ( ASL, 6, ZeroPageX )
+  0x1e -> ( ASL, 7, AbsoluteX )
+  0x90 -> ( BCC, 2, Relative )
+  0xb0 -> ( BCS, 2, Relative )
+  0xf0 -> ( BEQ, 2, Relative )
+  0x24 -> ( BIT, 3, ZeroPage )
+  0x2c -> ( BIT, 4, Absolute )
+  0x30 -> ( BMI, 2, Relative )
+  0xd0 -> ( BNE, 2, Relative )
+  0x10 -> ( BPL, 2, Relative )
+  0x00 -> ( BRK, 7, Implied )
+  0x50 -> ( BVC, 2, Relative )
+  0x70 -> ( BVS, 2, Relative )
+  0x18 -> ( CLC, 2, Implied )
+  0xd8 -> ( CLD, 2, Implied )
+  0x58 -> ( CLI, 2, Implied )
+  0xb8 -> ( CLV, 2, Implied )
+  0xc9 -> ( CMP, 2, Immediate )
+  0xc5 -> ( CMP, 3, ZeroPage )
+  0xcd -> ( CMP, 4, Absolute )
+  0xdd -> ( CMP, 4, AbsoluteX )
+  0xd9 -> ( CMP, 4, AbsoluteY )
+  0xd5 -> ( CMP, 4, ZeroPageX )
+  0xd1 -> ( CMP, 5, IndirectIndexedY )
+  0xc1 -> ( CMP, 6, IndexedIndirectX )
+  0xe0 -> ( CPX, 2, Immediate )
+  0xe4 -> ( CPX, 3, ZeroPage )
+  0xec -> ( CPX, 4, Absolute )
+  0xc0 -> ( CPY, 2, Immediate )
+  0xc4 -> ( CPY, 3, ZeroPage )
+  0xcc -> ( CPY, 4, Absolute )
+  0xc6 -> ( DEC, 5, ZeroPage )
+  0xce -> ( DEC, 6, Absolute )
+  0xd6 -> ( DEC, 6, ZeroPageX )
+  0xde -> ( DEC, 7, AbsoluteX )
+  0xca -> ( DEX, 2, Implied )
+  0x88 -> ( DEY, 2, Implied )
+  0x49 -> ( EOR, 2, Immediate )
+  0x45 -> ( EOR, 3, ZeroPage )
+  0x4d -> ( EOR, 4, Absolute )
+  0x5d -> ( EOR, 4, AbsoluteX )
+  0x59 -> ( EOR, 4, AbsoluteY )
+  0x55 -> ( EOR, 4, ZeroPageX )
+  0x51 -> ( EOR, 5, IndirectIndexedY )
+  0x41 -> ( EOR, 6, IndexedIndirectX )
+  0xe6 -> ( INC, 5, ZeroPage )
+  0xee -> ( INC, 6, Absolute )
+  0xf6 -> ( INC, 6, ZeroPageX )
+  0xfe -> ( INC, 7, AbsoluteX )
+  0xe8 -> ( INX, 2, Implied )
+  0xc8 -> ( INY, 2, Implied )
+  0x4c -> ( JMP, 3, Absolute )
+  0x6c -> ( JMP, 5, Indirect )
+  0x20 -> ( JSR, 6, Absolute )
+  0xa9 -> ( LDA, 2, Immediate )
+  0xa5 -> ( LDA, 3, ZeroPage )
+  0xad -> ( LDA, 4, Absolute )
+  0xbd -> ( LDA, 4, AbsoluteX )
+  0xb9 -> ( LDA, 4, AbsoluteY )
+  0xb5 -> ( LDA, 4, ZeroPageX )
+  0xb1 -> ( LDA, 5, IndirectIndexedY )
+  0xa1 -> ( LDA, 6, IndexedIndirectX )
+  0xa2 -> ( LDX, 2, Immediate )
+  0xa6 -> ( LDX, 3, ZeroPage )
+  0xae -> ( LDX, 4, Absolute )
+  0xbe -> ( LDX, 4, AbsoluteY )
+  0xb6 -> ( LDX, 4, ZeroPageY )
+  0xa0 -> ( LDY, 2, Immediate )
+  0xa4 -> ( LDY, 3, ZeroPage )
+  0xac -> ( LDY, 4, Absolute )
+  0xbc -> ( LDY, 4, AbsoluteX )
+  0xb4 -> ( LDY, 4, ZeroPageX )
+  0x4a -> ( LSR, 2, Accumulator )
+  0x46 -> ( LSR, 5, ZeroPage )
+  0x4e -> ( LSR, 6, Absolute )
+  0x56 -> ( LSR, 6, ZeroPageX )
+  0x5e -> ( LSR, 7, AbsoluteX )
+  0xea -> ( NOP, 2, Implied )
+  0x09 -> ( ORA, 2, Immediate )
+  0x05 -> ( ORA, 3, ZeroPage )
+  0x0d -> ( ORA, 4, Absolute )
+  0x1d -> ( ORA, 4, AbsoluteX )
+  0x19 -> ( ORA, 4, AbsoluteY )
+  0x15 -> ( ORA, 4, ZeroPageX )
+  0x11 -> ( ORA, 5, IndirectIndexedY )
+  0x01 -> ( ORA, 6, IndexedIndirectX )
+  0x48 -> ( PHA, 3, Implied )
+  0x08 -> ( PHP, 3, Implied )
+  0x68 -> ( PLA, 4, Implied )
+  0x28 -> ( PLP, 4, Implied )
+  0x2a -> ( ROL, 2, Accumulator )
+  0x26 -> ( ROL, 5, ZeroPage )
+  0x2e -> ( ROL, 6, Absolute )
+  0x36 -> ( ROL, 6, ZeroPageX )
+  0x3e -> ( ROL, 7, AbsoluteX )
+  0x6a -> ( ROR, 2, Accumulator )
+  0x66 -> ( ROR, 5, ZeroPage )
+  0x6e -> ( ROR, 6, Absolute )
+  0x76 -> ( ROR, 6, ZeroPageX )
+  0x7e -> ( ROR, 7, AbsoluteX )
+  0x40 -> ( RTI, 6, Implied )
+  0x60 -> ( RTS, 6, Implied )
+  0xe9 -> ( SBC, 2, Immediate )
+  0xe5 -> ( SBC, 3, ZeroPage )
+  0xed -> ( SBC, 4, Absolute )
+  0xfd -> ( SBC, 4, AbsoluteX )
+  0xf9 -> ( SBC, 4, AbsoluteY )
+  0xf5 -> ( SBC, 4, ZeroPageX )
+  0xf1 -> ( SBC, 5, IndirectIndexedY )
+  0xe1 -> ( SBC, 6, IndexedIndirectX )
+  0x38 -> ( SEC, 2, Implied )
+  0xf8 -> ( SED, 2, Implied )
+  0x78 -> ( SEI, 2, Implied )
+  0x85 -> ( STA, 3, ZeroPage )
+  0x8d -> ( STA, 4, Absolute )
+  0x95 -> ( STA, 4, ZeroPageX )
+  0x9d -> ( STA, 5, AbsoluteX )
+  0x99 -> ( STA, 5, AbsoluteY )
+  0x81 -> ( STA, 6, IndexedIndirectX )
+  0x91 -> ( STA, 6, IndirectIndexedY )
+  0x86 -> ( STX, 3, ZeroPage )
+  0x8e -> ( STX, 4, Absolute )
+  0x96 -> ( STX, 4, ZeroPageY )
+  0x84 -> ( STY, 3, ZeroPage )
+  0x8c -> ( STY, 4, Absolute )
+  0x94 -> ( STY, 4, ZeroPageX )
+  0xaa -> ( TAX, 2, Implied )
+  0xa8 -> ( TAY, 2, Implied )
+  0xba -> ( TSX, 2, Implied )
+  0x8a -> ( TXA, 2, Implied )
+  0x9a -> ( TXS, 2, Implied )
+  0x98 -> ( TYA, 2, Implied )
+  opcode ->
+    error (printf "unknown opcode: %02x" opcode)
