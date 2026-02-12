836c1ce07b241b3523567911bcb9290de0b454b3
Nick Chapman <nic@amadido.co.uk>
2026-02-12 08:42:20 +0000

    jenga test-nestest: reinstate PPU x/y in golden

--- src/CPU.hs
+++ src/CPU.hs
@@ -351,10 +351,14 @@ seeState State{a,x,y,flags,sp} = do
   flags <- read flags
   sp <- read sp
   cyc <- Cycles
+  let ppuCYC = cyc*3
+  let ppuCyclesPerScanLine = 341
+  let ppuX :: Int = ppuCYC `mod` ppuCyclesPerScanLine
+  let ppuY :: Int = ppuCYC `div` ppuCyclesPerScanLine
   let
     mes :: String =
-      printf "A:%02X X:%02X Y:%02X P:%02X SP:%02X  CYC:%d"
-       a x y flags sp cyc
+      printf "A:%02X X:%02X Y:%02X P:%02X SP:%02X PPU:%3d,%3d CYC:%d"
+       a x y flags sp ppuY ppuX cyc
   pure mes
 
 ----------------------------------------------------------------------
